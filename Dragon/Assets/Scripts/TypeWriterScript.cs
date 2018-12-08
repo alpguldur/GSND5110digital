7864ae4f012d0d9b4582994e538f08ebce7041f1
mghjin <kim.jinh@husky.neu.edu>
2018-12-08 16:46:02 -0600

    updated title and intro scenes

---
+++ Dragon/Assets/Scripts/TypeWriterScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterScript : MonoBehaviour {
+
+    Text imugiText;
+    string imugiStory;
+    public Button DescendButton;
+
+    void Awake()
+    {
+        DescendButton.gameObject.SetActive(false);
+        imugiText = GetComponent<Text>();
+        imugiStory = imugiText.text;
+        imugiText.text = "";
+        // TODO: add optional delay when to start
+        StartCoroutine("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in imugiStory)
+        {
+            imugiText.text += c;
+            yield return new WaitForSeconds(0.04f);
+            
+        }
+        // after interating through entire text
+        // show the descend button
+        DescendButton.gameObject.SetActive(true);
+    }
+
+       
+}
+        
