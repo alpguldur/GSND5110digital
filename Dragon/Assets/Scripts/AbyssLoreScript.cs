e8685985320b3a361c74963971060935b6b2ffb5
mghjin <kim.jinh@husky.neu.edu>
2018-12-11 20:21:12 -0600

    add ShowLore() scripts and evil dwayne asset

---
+++ Dragon/Assets/Scripts/AbyssLoreScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbyssLoreScript : MonoBehaviour
+{
+
+    public Text Lore_EvilDwayne;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Lore_EvilDwayne.gameObject.SetActive(true);
+            Start();
+        }
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("DisableText", 15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Lore_EvilDwayne.gameObject.SetActive(false);
+    }
+}
---
+++ Dragon/Assets/Scripts/ShallowsLoreScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoreObjectScript : MonoBehaviour {
+    public Text FoundKkwaengariLore;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FoundKkwaengariLore.gameObject.SetActive(true);
+            Start();
+        }
+    }
+    private void Start()
+    {
+        Invoke("DisableText", 15f);
+    }
+
+    void DisableText()
+    {
+        FoundKkwaengariLore.gameObject.SetActive(false);
+    }
+}
