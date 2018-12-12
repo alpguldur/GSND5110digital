b25a9424a7629c62cf5e9b9de02d9dfa28806e8b
mghjin <kim.jinh@husky.neu.edu>
2018-12-12 15:14:32 -0600

    add final lore final vers

--- Dragon/Assets/Scripts/AbyssLoreScript.cs
+++ Dragon/Assets/Scripts/AbyssLoreScript.cs
@@ -24,7 +24,7 @@ public class AbyssLoreScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void DisableText()
     {
         Lore_EvilDwayne.gameObject.SetActive(false);
     }
